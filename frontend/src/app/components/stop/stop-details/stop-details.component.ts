5ecb1353799015a29d27b464c6e306fbb788a42f
Fat Halimi <fat.halimi@i-solve.info>
2025-06-11 15:58:03 +0200

    minor fixes

--- frontend/src/app/components/stop/stop-details/stop-details.component.ts
+++ frontend/src/app/components/stop/stop-details/stop-details.component.ts
@@ -1,9 +1,8 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { Stop } from '../../../models/stop.model';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-stop-details',
@@ -16,25 +15,11 @@ import { HttpClient } from '@angular/common/http';
     MatButtonModule
   ]
 })
-export class StopDetailsComponent implements OnInit {
-  cityName: string = '';
-
+export class StopDetailsComponent {
   constructor(
     public dialogRef: MatDialogRef<StopDetailsComponent>,
-    @Inject(MAT_DIALOG_DATA) public stop: Stop,
-    private http: HttpClient
+    @Inject(MAT_DIALOG_DATA) public stop: Stop
   ) { }
-
-  ngOnInit(): void {
-    this.getCityFromCoordinates(this.stop.x, this.stop.y);
-  }
-
-  getCityFromCoordinates(x: number, y: number): void {
-    this.http.get<any>(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${y}&lon=${x}`)
-      .subscribe(result => {
-        this.cityName = result.display_name;
-      });
-  }
 
   onClose(): void {
     this.dialogRef.close();
